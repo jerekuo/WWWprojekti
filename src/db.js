94df58ad6c1d55438358e7ff143fda1989b85312
jerekuo <60038998+jerekuo@users.noreply.github.com>
2020-12-14 15:54:05 +0200

    Initial commit

---
+++ src/db.js
@@ -0,0 +1,13 @@
+const mongoose = require("mongoose");
+var dbUrl =
+  "mongodb+srv://admin:admin@cluster0.nk8k3.mongodb.net/microblog?retryWrites=true&w=majority";
+mongoose
+  .connect(dbUrl, { useNewUrlParser: true })
+  .catch((err) => console.log(err));
+mongoose.connection
+  .once("open", function () {
+    console.log("Connected");
+  })
+  .on("error", function (error) {
+    console.log("There's an error", error);
+  });
